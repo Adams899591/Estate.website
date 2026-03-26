377a471706042ac2d998ee1329ee6a5d248246d4
Adams899591 <usmanadams551@gmail.com>
2026-03-26 05:25:58 +0100

    analytices and settings

--- app/Http/Controllers/Admin/AnalyticsController.php
+++ app/Http/Controllers/Admin/AnalyticsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Admin\ShowAnalyticsPageResource;
+use App\Models\Property;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,6 +12,20 @@ class AnalyticsController extends Controller
 {
     //show Analytics Page
     public function showAnalyticsPage(){
-        return Inertia::render("Admin/Analytics");
+
+      // analytics data for property types and status 
+       $analytics = [
+
+            "commercial" => Property::where("type","Commercial")->count(),
+            "house" => Property::where("type","House")->count(),
+            "apartment" => Property::where("type","Apartment")->count(),
+            "forSale" => Property::where("status","For Sale")->count(),
+            "forRent" => Property::where("status","For Rent")->count(),
+            "sold" => Property::where("status","Sold")->count(),
+       ];
+
+
+    
+        return Inertia::render("Admin/Analytics",["analytics" => $analytics]);
     }
 }
